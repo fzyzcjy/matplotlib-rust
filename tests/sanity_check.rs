ddd0bf7cf98edd067a44433b8d45958965d33bc1
fzyzcjy <ch271828n@outlook.com>
2021-10-09 09:12:44 +0800

    minor change

--- tests/sanity_check.rs
+++ tests/sanity_check.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use anyhow::Result;
 use pyo3::prelude::*;
 use pyo3::types::IntoPyDict;
@@ -20,32 +22,32 @@ fn python_sanity_check() -> Result<()> {
 #[test]
 fn matplotlib_sanity_check() -> Result<()> {
     Python::with_gil(|py| -> Result<()> {
-        // println!("call import matplotlib");
-        // let matplotlib = py.import("matplotlib")?;
-        // println!("result {:?}", matplotlib);
-        //
-        // println!("call get_backend");
-        // println!("result {:?}", matplotlib.call_method0("get_backend")?);
-        //
-        // println!("call version");
-        // println!("result {:?}", matplotlib.getattr("__version__")?);
+        println!("call import matplotlib");
+        let matplotlib = py.import("matplotlib")?;
+        println!("result {:?}", matplotlib);
 
-        println!(
-            "result {:?}",
-            py.run(
-                "\
-import matplotlib.pyplot as plt;
-plt.ion();
-print('after import');
-plt.plot([10,20], [100,110]);
-print('after plot');
-plt.show();
-print('after show');
-            ",
-                None,
-                None,
-            )?
-        );
+        println!("call get_backend");
+        println!("result {:?}", matplotlib.call_method0("get_backend")?);
+
+        println!("call version");
+        println!("result {:?}", matplotlib.getattr("__version__")?);
+
+        //         println!(
+        //             "result {:?}",
+        //             py.run(
+        //                 "\
+        // import matplotlib.pyplot as plt;
+        // plt.ion();
+        // print('after import');
+        // plt.plot([10,20], [100,110]);
+        // print('after plot');
+        // plt.show();
+        // print('after show');
+        //             ",
+        //                 None,
+        //                 None,
+        //             )?
+        //         );
 
         // println!(
         //     "result {:?}",
@@ -54,18 +56,34 @@ print('after show');
         // println!("result {:?}", py.run("plt.plot(10,20)", None, None)?);
         // println!("result {:?}", py.run("plt.show()", None, None)?);
 
-        // println!("call import plt");
-        // let plt = py.import("matplotlib.pyplot")?;
-        // println!("result {:?}", plt);
-        //
-        // println!("call plot");
-        // println!(
-        //     "result {:?}",
-        //     plt.call_method1("plot", ([10, 100], [20, 200]))?
-        // );
-        //
-        // println!("call show");
-        // println!("result {:?}", plt.call_method0("show")?);
+        println!("call import plt");
+        let plt = py.import("matplotlib.pyplot")?;
+        println!("result {:?}", plt);
+
+        println!("call ion");
+        println!("result {:?}", plt.call_method0("ion")?);
+
+        println!("call plot");
+        println!(
+            "result {:?}",
+            plt.call_method1("plot", ([10, 20], [0, 100]))?
+        );
+
+        println!("call show");
+        println!("result {:?}", plt.call_method0("show")?);
+
+        for i in 0..3 {
+            std::thread::sleep(Duration::from_millis(500));
+
+            println!("call plot again");
+            println!(
+                "result {:?}",
+                plt.call_method1("plot", ([10, (i + 2) * 20], [0, 100]))?
+            );
+
+            println!("call show");
+            println!("result {:?}", plt.call_method0("show")?);
+        }
 
         Ok(())
     })
