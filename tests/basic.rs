7d49ba275890429c577e0e5227cc69faaf2bceb9
fzyzcjy <ch271828n@outlook.com>
2021-10-09 08:23:39 +0800

    no need for conda workaround

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,23 +1,12 @@
 use anyhow::Result;
+use libc::wchar_t;
 use pyo3::ffi;
 use pyo3::prelude::*;
 use pyo3::types::IntoPyDict;
 use widestring::WideCString;
 
-use libc::wchar_t;
-
 #[test]
 fn python_sanity_check() -> Result<()> {
-    if let Some(python_home) = std::env::var_os("CONDA_PREFIX") {
-        unsafe {
-            ffi::Py_SetPythonHome(
-                WideCString::from_str(python_home.to_str().unwrap())
-                    .unwrap()
-                    .as_ptr() as *const wchar_t,
-            );
-        }
-    }
-
     Python::with_gil(|py| {
         let sys = py.import("sys")?;
         let version: String = sys.getattr("version")?.extract()?;
